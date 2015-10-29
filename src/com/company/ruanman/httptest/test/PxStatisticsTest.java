18d4f11186f031c13addd7b66fa40bab89225e87
yjjewg11 <yjjewg11>
2015-10-29 17:31:51 +0800

    u

---
+++ src/com/company/ruanman/httptest/test/PxStatisticsTest.java
@@ -0,0 +1,80 @@
+package com.company.ruanman.httptest.test;
+
+import java.io.ByteArrayInputStream;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import net.sf.json.JSONObject;
+
+import com.company.news.SystemConstants;
+import com.company.news.jsonform.GroupRegJsonform;
+import com.company.news.jsonform.StudentJsonform;
+import com.company.news.jsonform.UserRegJsonform;
+import com.company.news.rest.RestConstants;
+import com.company.news.rest.util.MD5Until;
+import com.company.news.rest.util.TimeUtils;
+import com.company.ruanman.httptest.AbstractHttpTest;
+import com.company.ruanman.httptest.HttpUtils;
+import com.company.ruanman.httptest.JSONUtils;
+import com.company.ruanman.httptest.TestConstants;
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.PostMethodWebRequest;
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebResponse;
+
+public class PxStatisticsTest extends AbstractHttpTest {
+	  public UserinfoTest user= new UserinfoTest();
+	/**
+	 * run this testcase as a suite from the command line
+	 * 
+	 * @param args
+	 *            - ignored
+	 * @throws Exception
+	 */
+	public static void main(String args[]) throws Exception {
+		// junit.textui.TestRunner.run( suite() );
+		
+		PxStatisticsTest o = new PxStatisticsTest();
+		// o.testRegSuccess();
+		o.testSuccess("uss");
+		o.testSuccess("uls");
+		o.testSuccess("sss");
+		o.testSuccess("css");
+
+        //o.testGetSuccess();
+		//o.testAddSuccess();
+	}
+
+	/**
+	 * supply this test cases as part of a suite
+	 * 
+	 * @return
+	 */
+	public static Test suite() {
+		return new TestSuite(PxStatisticsTest.class);
+	}
+
+
+
+
+	
+	
+	
+	public void testSuccess(String path) throws Exception {
+		WebConversation conversation = new WebConversation();
+		// GetMethodWebRequest
+		WebRequest request = new GetMethodWebRequest(TestConstants.host
+				+ "rest/pxstatistics/"+path+".json"+user.addParameter_JSESSIONID()
+				+"&groupuuid=group_px1&begDateStr=2015-07-11&endDateStr=2015-10-11");
+
+		WebResponse response = tryGetResponse(conversation, request);
+
+		HttpUtils.println(conversation, request, response);
+		assertTrue("成功", response.getText().indexOf("success") != -1);
+
+	}
+	
+
+
+}
